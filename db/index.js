421bfd1019fd4cae964263fb7aff035a537b860e
Will Garcia <wjgarcia2013@gmail.com>
2018-07-22 19:29:55 -0400

    Centralize Backend Configs
    Instead of having process.env references in multiple files it is much more maintanable to have them all in a centralized location.  Also, instead of setting the dev config a home directory JSON file the package dotenv() is introduced to allow .env files to drive process.env environment variables.

--- db/index.js
+++ db/index.js
@@ -1,35 +1,47 @@
 'use strict'
-const app = require('APP')
-    , debug = require('debug')(`${app.name}:db`) // DEBUG=your_app_name:db
-    , chalk = require('chalk')
-    , Sequelize = require('sequelize')
-
-    , name = (app.env.DATABASE_NAME || app.name) +
-             (app.isTesting ? '_test' : '')
-    , url = app.env.DATABASE_URL || `postgres://localhost:5432/${name}`
+const {
+  isTesting,
+  isProduction,
+  appName,
+  databaseDomain,
+  databasePort,
+  databaseName,
+  databaseUsername,
+  databasePassword
+} = require('../config')
+const debug = require('debug')(`${appName}:db`) // DEBUG=your_app_name:db
+const chalk = require('chalk')
+const Sequelize = require('sequelize')
+const name = (databaseName || appName) + (isTesting ? '_test' : '')
+const url =
+  `postgres://${databaseUsername}:${databasePassword}@${databaseDomain}:${databasePort}/${databaseName}` ||
+  `postgres://@localhost:5432/${name}`
 
 debug(chalk.yellow(`Opening database connection to ${url}`))
-const db = module.exports = new Sequelize(url, {
-  logging: require('debug')('sql'),  // export DEBUG=sql in the environment to
-                                     // get SQL queries
+const db = (module.exports = new Sequelize(url, {
+  logging: require('debug')('sql'), // export DEBUG=sql in the environment to
+  // get SQL queries
   define: {
-    underscored: true,       // use snake_case rather than camelCase column names.
-                             // these are easier to work with in psql.
-    freezeTableName: true,   // don't change table names from the one specified
-    timestamps: true,        // automatically include timestamp columns
+    underscored: true, // use snake_case rather than camelCase column names.
+    // these are easier to work with in psql.
+    freezeTableName: true, // don't change table names from the one specified
+    timestamps: true // automatically include timestamp columns
   }
-})
+}))
 
 // Initialize all our models and assign them as properties
 // on the database object.
 //
 // This lets us use destructuring to get at them like so:
 //
-//   const {User, Product} = require('APP/db')
+//   const {User, Product} = require('../db')
 //
-Object.assign(db, require('./models')(db),
+Object.assign(
+  db,
+  require('./models')(db),
   // We'll also make createAndSync available. It's sometimes useful in tests.
-  {createAndSync})
+  { createAndSync }
+)
 
 // After defining all the models, sync the database.
 // Notice that didSync *is* a Promise, rather than being a function that returns
@@ -37,13 +49,14 @@ Object.assign(db, require('./models')(db),
 db.didSync = db.createAndSync()
 
 // sync the db, creating it if necessary
-function createAndSync(force=app.isTesting, retries=0, maxRetries=5) {
-  return db.sync({force})
+function createAndSync(force = isTesting, retries = 0, maxRetries = 5) {
+  return db
+    .sync({ force })
     .then(() => debug(`Synced models to db ${url}`))
     .catch(fail => {
       // Don't do this auto-create nonsense in prod, or
       // if we've retried too many times.
-      if (app.isProduction || retries > maxRetries) {
+      if (isProduction || retries > maxRetries) {
         console.error(chalk.red(`********** database error ***********`))
         console.error(chalk.red(`    Couldn't connect to ${url}`))
         console.error()
@@ -52,7 +65,9 @@ function createAndSync(force=app.isTesting, retries=0, maxRetries=5) {
         return
       }
       // Otherwise, do this autocreate nonsense
-      debug(`${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`)
+      debug(
+        `${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`
+      )
       return new Promise(resolve =>
         // 'child_process.exec' docs: https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
         require('child_process').exec(`createdb "${name}"`, resolve)
