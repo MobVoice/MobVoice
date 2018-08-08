0393d9773fd81286def7ce120cfc69fef8ae3c9d
Strangebrewer <BKAShambala@gmail.com>
2018-08-07 18:47:15 -0600

    poking around - redesign login and signup

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -4,17 +4,23 @@ export const Login = ({ login }) => (
   <form onSubmit={evt => {
     evt.preventDefault()
     login(evt.target.username.value, evt.target.password.value)
-  } }>
-    <input name="username" />
-    <input name="password" type="password" />
-    <input type="submit" value="Login" />
+  }}>
+    <div>
+      <h4>Name:</h4>
+      <input style={{width: '100%'}} name="username" />
+    </div>
+    <div>
+      <h4>Password:</h4>
+      <input style={{width: '100%'}} name="password" type="password" />
+    </div>
+    <input style={{marginTop: '10px'}} type="submit" value="Login" />
   </form>
 )
 
-import {login} from '../reducers/auth'
-import {connect} from 'react-redux'
+import { login } from '../reducers/auth'
+import { connect } from 'react-redux'
 
 export default connect(
   state => ({}),
-  {login},
+  { login },
 )(Login)
--- app/components/LoginSignUp.jsx
+++ app/components/LoginSignUp.jsx
@@ -8,21 +8,21 @@ import axios from 'axios'
 class LoginSignUp extends React.Component {
   constructor(props) {
     super(props)
-    this.state={value: '', error: ''}
+    this.state = { value: '', error: '', showLogin: true }
     this.onSignupSubmit = this.onSignupSubmit.bind(this)
-    this.onChange=this.onChange.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
 
   onChange(event) {
-    this.setState({value: event.target.value})
+    this.setState({ value: event.target.value })
   }
 
   onSignupSubmit(evt) {
     evt.preventDefault()
     if (!this.is50Char(evt.target.email.value) || !this.is26Char(evt.target.name.value)) {
-      this.setState({error: 'Name or Email must be less than 26 characters.'})
+      this.setState({ error: 'Name or Email must be less than 26 characters.' })
     } else {
-      this.props.addNewUser({name: evt.target.name.value, isTeacher: true, email: evt.target.email.value, password: evt.target.password.value})
+      this.props.addNewUser({ name: evt.target.name.value, isTeacher: true, email: evt.target.email.value, password: evt.target.password.value })
     }
   }
 
@@ -34,54 +34,108 @@ class LoginSignUp extends React.Component {
     return str.length < 50
   }
 
+  toggleForm = () => {
+    this.setState({
+      showLogin: !this.state.showLogin
+    })
+  }
+
   render() {
+    const formContainerStyle = {
+      maxWidth: '370px',
+      width: '100%',
+      margin: '130px auto',
+      padding: '0',
+      border: '1px solid #000',
+      backgroundColor: '#fff'
+    }
+    const toggleBtnStyle = {
+      width: '50%',
+      backgroundColor: '#000',
+      color: '#ddd',
+      padding: '10px 0',
+      border: 'none',
+      outline: 'transparent'
+    }
+    const toggleBtnLightStyle = {
+      width: '50%',
+      backgroundColor: '#000',
+      color: '#ddd',
+      padding: '10px 0',
+      border: 'none',
+      outline: 'transparent',
+      opacity: '.6'
+    }
+
     return (
       <div>
-        <a target="_self" href="/api/auth/login/facebook"><i/>Sign in with Facebook.</a>
-        <h4>LOGIN</h4>
-        <Login/>
-        <br></br>
-        <h4>SIGN UP</h4>
-        <div>
-          <form onSubmit={this.onSignupSubmit}>
-            <div>
-              <h4>Name:</h4>
-              <input
-                name="name"
-                type="text"
-                onChange={this.onChange}
-                required
-              />
-            </div>
-            <div>
-              <h4>Email:</h4>
-              <input
-                name="email"
-                type="email"
-                required
-              />
-            </div>
-            <div>
-              <h4>Password:</h4>
-              <input
-                name="password"
-                type="password"
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-block btn-primary">Submit</button>
-          </form>
-          {this.state.error ? <p style={{color: 'red'}}>{this.state.error}</p> : null}
+        <a target="_self" href="/api/auth/login/facebook"><i />Sign in with Facebook.</a>
+        <div style={formContainerStyle}>
+          {this.state.showLogin
+            ? (
+              <React.Fragment>
+                <div style={{display: 'flex', border: '1px solid #000'}}>
+                  <button style={toggleBtnLightStyle} onClick={this.toggleForm}>Signup</button>
+                  <button style={toggleBtnStyle }>Login</button>
+                </div>
+                <div style={{border: '1px solid #000', padding: '20px 30px'}}>
+                  <h4 style={{marginBottom: '20px'}}>LOGIN</h4>
+                  <Login />
+                </div>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <div style={{display: 'flex', border: '1px solid #000'}}>
+                  <button style={toggleBtnStyle}>Signup</button>
+                  <button style={toggleBtnLightStyle} onClick={this.toggleForm} >Login</button>
+                </div>
+                <div style={{border: '1px solid #000', padding: '20px 30px'}}>
+                  <h4 style={{marginBottom: '20px'}}>SIGN UP</h4>
+                  <form onSubmit={this.onSignupSubmit}>
+                    <div>
+                      <h4>Name:</h4>
+                      <input
+                        style={{width: '100%'}}
+                        name="name"
+                        type="text"
+                        onChange={this.onChange}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <h4>Email:</h4>
+                      <input
+                        style={{width: '100%'}}
+                        name="email"
+                        type="email"
+                        required
+                      />
+                    </div>
+                    <div>
+                      <h4>Password:</h4>
+                      <input
+                        style={{width: '100%'}}
+                        name="password"
+                        type="password"
+                        required
+                      />
+                    </div>
+                    <button style={{marginTop: '10px'}} type="submit" className="btn btn-block btn-primary">Submit</button>
+                  </form>
+                  {this.state.error ? <p style={{ color: 'red' }}>{this.state.error}</p> : null}
+                </div>
+              </React.Fragment>
+            )}
         </div>
       </div>
     )
   }
 }
 
-import {addNewUser} from '../reducers/auth'
-import {connect} from 'react-redux'
+import { addNewUser } from '../reducers/auth'
+import { connect } from 'react-redux'
 
 export default connect(
   state => ({}),
-  {addNewUser},
+  { addNewUser },
 )(LoginSignUp)
